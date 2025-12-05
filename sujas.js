4f05d6ea917280690b544cbde6b4a61ae70bd712
musfikurzony <musfikurzony@gmail.com>
2025-12-05 12:24:48 +0600

    Update sujas.js

--- sujas.js
+++ sujas.js
@@ -1,21 +1,7 @@
-const surahFiles = [
-  { number: 101, file: 'data/surah_101.json' }
-];
-
-function loadSurahList() {
-  const container = document.getElementById('surah-list');
-  container.innerHTML = '';
-  surahFiles.forEach(surah => {
-    const div = document.createElement('div');
-    div.className = 'surah';
-    div.textContent = `Surah ${surah.number} - ${surah.number===101?'আল-ক্বারিয়াহ্':''}`;
-    div.onclick = () => loadSurah(surah.file);
-    container.appendChild(div);
-  });
-}
-
-async function loadSurah(file) {
-  const res = await fetch(file);
-  const surah = await res.json();
-  displayAyat(surah);
-}
+// Currently only Surah 101, extend later
+const suras = {
+  "101": {
+    "name": "আল-ক্বারিয়াহ্",
+    "ayaat": 11
+  }
+};
